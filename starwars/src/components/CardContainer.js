031ac8938945a2cc4f21e2f7b0c97b3ef4146880
Josiah <reikiryo@yahoo.com>
2020-01-24 13:47:57 -0500

    Stretch goal complete *Creates next and back buttons *Next and back buttons update the characters *Creates new state next, back, and page to accomplish this

--- starwars/src/components/CardContainer.js
+++ starwars/src/components/CardContainer.js
@@ -1,4 +1,4 @@
-import React, {useState,useEffect} from 'react';
+import React, {useState,useEffect} from 'react'
 import styled from 'styled-components'
 import Cards from './Cards'
 import axios from 'axios'
@@ -12,23 +12,78 @@ const ContainerDiv = styled.div`
 	border: 2px solid white;
 `
 
-export default function Container(props) {
+const ButtonDiv = styled.div`
+    width: 50%;
+    height: 5vh;
+    border: 2px solid white;
+    margin: 0 auto;
+`
+const Button = styled.button`
+    width: 40%;
+    height: 100%;
+`
+
+// const getCharacters = nextOrBack => {
+// 	console.log('hi i work');
+// }
+
+export default function Container() {
 	const [data, setData] = useState([])
+	const [page, setPage] = useState('https://swapi.co/api/people/')
+	const [next, setNext] = useState('')
+	const [back, setBack] = useState('')
 	useEffect(() => {
-		axios.get('https://swapi.co/api/people/')
+		axios.get(page)
 			.then(res => {
-				console.log(res.data.results)
+				console.log(res)
 				setData(res.data.results)
+				setNext(res.data.next)
+				setBack(res.data.previous)
 			})
 			.catch(err => {
 				console.log('Sorry bro no data this time', err)
 			})
-	}, [])
+	}, [page])
+
+	// const getCharacters = (nextOrBack) => {
+	// 	if (nextOrBack === 1 && back !== null) {
+	// 		console.log('NUM 1 WORKING')
+	// 		axios.get(back)
+	// 				.then(res => {
+	// 					console.log(res)
+	// 					setData(res.data.results)
+	// 					setNext(res.data.next)
+	// 					setBack(res.data.previous)
+	// 				})
+	// 				.catch(err => {
+	// 					console.log('Sorry bro no data this time', err)
+	// 				})
+	// 	} else if (nextOrBack === 2 && next !== null) {
+	// 		console.log('NUM 2 WORKING')
+	// 		axios.get(next)
+	// 				.then(res => {
+	// 					console.log(res)
+	// 					setData(res.data.results)
+	// 					setNext(res.data.next)
+	// 					setBack(res.data.previous)
+	// 				})
+	// 				.catch(err => {
+	// 					console.log('Sorry bro no data this time', err)
+	// 				})
+	// 	}
+	// }
+
 	return (
+		<div>
+		<ButtonDiv>
+            <Button onClick={() => setPage(back)} key='1'>Back</Button>
+            <Button onClick={() => setPage(next)} key='2'>Next</Button>
+        </ButtonDiv>
 		<ContainerDiv>
 			{data.map(x => (
 				<Cards key={x.url} data={x} />
 			))}
 		</ContainerDiv>
+		</div>
 	)
 }
